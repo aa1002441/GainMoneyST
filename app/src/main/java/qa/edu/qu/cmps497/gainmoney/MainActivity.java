77adf26f152a7682be4ae37dd8198d6cc1179633
aa1002441 <aa1002441@qu.edu.qa>
2015-12-07 12:58:44 +0300

    Initial commit

---
+++ app/src/main/java/qa/edu/qu/cmps497/gainmoney/MainActivity.java
@@ -0,0 +1,13 @@
+package qa.edu.qu.cmps497.gainmoney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
